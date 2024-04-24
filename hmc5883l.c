ecaa9ca0340aa880ce83d9aa5c5fbe26aa86fa0b
phonght32 <thanhphongho1998@gmail.com>
2024-04-24 21:11:33 +0700

    handle magnetometer

--- hmc5883l.c
+++ hmc5883l.c
@@ -3,12 +3,226 @@
 #include "string.h"
 #include "hmc5883l.h"
 
+#define HMC5883L_REG_CONFIG_A         	0x00
+#define HMC5883L_REG_CONFIG_B         	0x01
+#define HMC5883L_REG_MODE             	0x02
+#define HMC5883L_REG_OUT_X_M          	0x03
+#define HMC5883L_REG_OUT_X_L          	0x04
+#define HMC5883L_REG_OUT_Z_M          	0x05
+#define HMC5883L_REG_OUT_Z_L          	0x06
+#define HMC5883L_REG_OUT_Y_M          	0x07
+#define HMC5883L_REG_OUT_Y_L          	0x08
+#define HMC5883L_REG_STATUS           	0x09
+#define HMC5883L_REG_IDENT_A          	0x0A
+#define HMC5883L_REG_IDENT_B          	0x0B
+#define HMC5883L_REG_IDENT_C          	0x0C
+
 typedef struct hmc5883l {
 	hmc5883l_range_t  			range;						/*!< Sensor field range */
 	hmc5883l_opr_mode_t  		opr_mode;  					/*!< Operating mode */
 	hmc5883l_data_rate_t  		data_rate;					/*!< Data output rate */
-	hmc5883l_sample_t  			sample;						/*!< Number of samples averaged */
+	hmc5883l_sample_t  			samples;					/*!< Number of samples averaged */
 	hmc5883l_func_i2c_send      i2c_send;        			/*!< HMC5883L send bytes */
 	hmc5883l_func_i2c_recv      i2c_recv;         			/*!< HMC5883L receive bytes */
 	hmc5883l_func_delay         delay;                 		/*!< HMC5883L delay function */
+	float 						mag_per_digit;				/*!< Mag/digit */
+	int 						mag_bias_x;  				/*!< Magnetometer bias x axis */
+	int 						mag_bias_y;  				/*!< Magnetometer bias y axis */
+	int 						mag_bias_z;  				/*!< Magnetometer bias z axis */
 } hmc5883l_t;
+
+hmc5883l_handle_t hmc5883l_init(void)
+{
+	hmc5883l_handle_t handle = calloc(1, sizeof(hmc5883l_t));
+	if (handle == NULL)
+	{
+		return NULL;
+	}
+
+	return handle;
+}
+
+err_code_t hmc5883l_set_config(hmc5883l_handle_t handle, hmc5883l_cfg_t config)
+{
+	/* Check if handle structure is NULL */
+	if (handle == NULL)
+	{
+		return ERR_CODE_NULL_PTR;
+	}
+
+	float mag_per_digit = 0;
+
+	switch (config.range)
+	{
+	case HMC5883L_RANGE_0_88GA:
+		mag_per_digit = 0.073f;
+		break;
+
+	case HMC5883L_RANGE_1_3GA:
+		mag_per_digit = 0.92f;
+		break;
+
+	case HMC5883L_RANGE_1_9GA:
+		mag_per_digit = 1.22f;
+		break;
+
+	case HMC5883L_RANGE_2_5GA:
+		mag_per_digit = 1.52f;
+		break;
+
+	case HMC5883L_RANGE_4_0GA:
+		mag_per_digit = 2.27f;
+		break;
+
+	case HMC5883L_RANGE_4_7GA:
+		mag_per_digit = 2.56f;
+		break;
+
+	case HMC5883L_RANGE_5_6GA:
+		mag_per_digit = 3.03f;
+		break;
+
+	case HMC5883L_RANGE_8_1GA:
+		mag_per_digit = 4.35f;
+		break;
+
+	default:
+		break;
+	}
+
+	handle->range = config.range;
+	handle->opr_mode = config.opr_mode;
+	handle->data_rate = config.data_rate;
+	handle->samples = config.samples;
+	handle->i2c_send = config.i2c_send;
+	handle->i2c_recv = config.i2c_recv;
+	handle->delay = config.delay;
+	handle->mag_per_digit = mag_per_digit;
+	handle->mag_bias_x = config.mag_bias_x;
+	handle->mag_bias_y = config.mag_bias_y;
+	handle->mag_bias_z = config.mag_bias_z;
+
+	return ERR_CODE_SUCCESS;
+}
+
+err_code_t hmc5883l_config(hmc5883l_handle_t handle)
+{
+	/* Check if handle structure is NULL */
+	if (handle == NULL)
+	{
+		return ERR_CODE_NULL_PTR;
+	}
+
+	uint8_t cmd_data = 0;
+
+	/* Set range */
+	cmd_data = handle->range << 5;
+	handle->i2c_send(HMC5883L_REG_CONFIG_B, &cmd_data, 1);
+
+	/* Set operating mode */
+	handle->i2c_recv(HMC5883L_REG_MODE, &cmd_data, 1);
+	cmd_data &= 0b11111100;
+	cmd_data |= handle->opr_mode;
+	handle->i2c_send(HMC5883L_REG_MODE, &cmd_data, 1);
+
+	/* Set data rate */
+	handle->i2c_recv(HMC5883L_REG_CONFIG_A, &cmd_data, 1);
+	cmd_data &= 0b11100011;
+	cmd_data |= (handle->data_rate << 2);
+	handle->i2c_send(HMC5883L_REG_CONFIG_A, &cmd_data, 1);
+
+	/* Set samples */
+	handle->i2c_recv(HMC5883L_REG_CONFIG_A, &cmd_data, 1);
+	cmd_data &= 0b10011111;
+	cmd_data |= (handle->samples << 5);
+	handle->i2c_send(HMC5883L_REG_CONFIG_A, &cmd_data, 1);
+
+	return ERR_CODE_SUCCESS;
+}
+
+err_code_t hmc5883l_get_mag_raw(hmc5883l_handle_t handle, int16_t *raw_x, int16_t *raw_y, int16_t *raw_z)
+{
+	/* Check if handle structure is NULL */
+	if (handle == NULL)
+	{
+		return ERR_CODE_NULL_PTR;
+	}
+
+	handle->i2c_recv(HMC5883L_REG_OUT_X_M, (uint8_t *)raw_x, 2);
+	handle->i2c_recv(HMC5883L_REG_OUT_Y_M, (uint8_t *)raw_y, 2);
+	handle->i2c_recv(HMC5883L_REG_OUT_Z_M, (uint8_t *)raw_z, 2);
+
+	return ERR_CODE_SUCCESS;
+}
+
+err_code_t hmc5883l_get_mag_calib(hmc5883l_handle_t handle, int16_t *calib_x, int16_t *calib_y, int16_t *calib_z)
+{
+	/* Check if handle structure is NULL */
+	if (handle == NULL)
+	{
+		return ERR_CODE_NULL_PTR;
+	}
+
+	int16_t raw_x = 0, raw_y = 0, raw_z = 0;
+
+	handle->i2c_recv(HMC5883L_REG_OUT_X_M, (uint8_t *)&raw_x, 2);
+	handle->i2c_recv(HMC5883L_REG_OUT_Y_M, (uint8_t *)&raw_y, 2);
+	handle->i2c_recv(HMC5883L_REG_OUT_Z_M, (uint8_t *)&raw_z, 2);
+
+	*calib_x = raw_x - handle->mag_bias_x;
+	*calib_y = raw_y - handle->mag_bias_y;
+	*calib_z = raw_z - handle->mag_bias_z;
+
+	return ERR_CODE_SUCCESS;
+}
+
+err_code_t hmc5883l_get_mag_scale(hmc5883l_handle_t handle, float *scale_x, float *scale_y, float *scale_z)
+{
+	/* Check if handle structure is NULL */
+	if (handle == NULL)
+	{
+		return ERR_CODE_NULL_PTR;
+	}
+
+	int16_t raw_x = 0, raw_y = 0, raw_z = 0;
+
+	handle->i2c_recv(HMC5883L_REG_OUT_X_M, (uint8_t *)&raw_x, 2);
+	handle->i2c_recv(HMC5883L_REG_OUT_Y_M, (uint8_t *)&raw_y, 2);
+	handle->i2c_recv(HMC5883L_REG_OUT_Z_M, (uint8_t *)&raw_z, 2);
+
+	*scale_x = (raw_x - handle->mag_bias_x) * handle->mag_per_digit;
+	*scale_y = (raw_y - handle->mag_bias_y) * handle->mag_per_digit;
+	*scale_z = (raw_z - handle->mag_bias_z) * handle->mag_per_digit;
+
+	return ERR_CODE_SUCCESS;
+}
+
+err_code_t hmc5883l_set_mag_bias(hmc5883l_handle_t handle, int16_t bias_x, int16_t bias_y, int16_t bias_z)
+{
+	/* Check if handle structure is NULL */
+	if (handle == NULL)
+	{
+		return ERR_CODE_NULL_PTR;
+	}
+
+	handle->mag_bias_x = bias_x;
+	handle->mag_bias_y = bias_y;
+	handle->mag_bias_z = bias_z;
+
+	return ERR_CODE_SUCCESS;
+}
+
+err_code_t hmc5883l_get_mag_bias(hmc5883l_handle_t handle, int16_t *bias_x, int16_t *bias_y, int16_t *bias_z)
+{
+	/* Check if handle structure is NULL */
+	if (handle == NULL)
+	{
+		return ERR_CODE_NULL_PTR;
+	}
+
+	*bias_x = handle->mag_bias_x;
+	*bias_y = handle->mag_bias_y;
+	*bias_z = handle->mag_bias_z;
+
+	return ERR_CODE_SUCCESS;
+}
--- hmc5883l.h
+++ hmc5883l.h
@@ -29,7 +29,7 @@ extern "C" {
 
 #include "err_code.h"
 
-#define HMC5883L_I2C_ADDR		(0x68)
+#define HMC5883L_I2C_ADDR		(0x1E)
 
 typedef err_code_t (*hmc5883l_func_i2c_send)(uint8_t reg_addr, uint8_t *buf_send, uint16_t len);
 typedef err_code_t (*hmc5883l_func_i2c_recv)(uint8_t reg_addr, uint8_t *buf_recv, uint16_t len);
@@ -93,10 +93,13 @@ typedef struct {
 	hmc5883l_range_t  			range;						/*!< Sensor field range */
 	hmc5883l_opr_mode_t  		opr_mode;  					/*!< Operating mode */
 	hmc5883l_data_rate_t  		data_rate;					/*!< Data output rate */
-	hmc5883l_sample_t  			sample;						/*!< Number of samples averaged */
+	hmc5883l_sample_t  			samples;					/*!< Number of samples averaged */
 	hmc5883l_func_i2c_send      i2c_send;        			/*!< HMC5883L send bytes */
 	hmc5883l_func_i2c_recv      i2c_recv;         			/*!< HMC5883L receive bytes */
 	hmc5883l_func_delay         delay;                 		/*!< HMC5883L delay function */
+	int 						mag_bias_x;  				/*!< Magnetometer bias x axis */
+	int 						mag_bias_y;  				/*!< Magnetometer bias y axis */
+	int 						mag_bias_z;  				/*!< Magnetometer bias z axis */
 } hmc5883l_cfg_t;
 
 /*
@@ -134,6 +137,76 @@ err_code_t hmc5883l_set_config(hmc5883l_handle_t handle, hmc5883l_cfg_t config);
  *      - Others:           Fail.
  */
 err_code_t hmc5883l_config(hmc5883l_handle_t handle);
+
+/*
+ * @brief   Get magnetometer raw value.
+ *
+ * @param   handle Handle structure.
+ * @param   raw_x Raw value x axis.
+ * @param   raw_y Raw value y axis.
+ * @param   raw_z Raw value z axis.
+ *
+ * @return
+ *      - ERR_CODE_SUCCESS: Success.
+ *      - Others:           Fail.
+ */
+err_code_t hmc5883l_get_mag_raw(hmc5883l_handle_t handle, int16_t *raw_x, int16_t *raw_y, int16_t *raw_z);
+
+/*
+ * @brief   Get magnetometer calibrated data.
+ *
+ * @param   handle Handle structure.
+ * @param   calib_x Calibrated data x axis.
+ * @param   calib_y Calibrated data y axis.
+ * @param   calib_z Calibrated data z axis.
+ *
+ * @return
+ *      - ERR_CODE_SUCCESS: Success.
+ *      - Others:           Fail.
+ */
+err_code_t hmc5883l_get_mag_calib(hmc5883l_handle_t handle, int16_t *calib_x, int16_t *calib_y, int16_t *calib_z);
+
+/*
+ * @brief   Get magnetometer scaled data.
+ *
+ * @param   handle Handle structure.
+ * @param   scale_x Scaled data x axis.
+ * @param   scale_y Scaled data y axis.
+ * @param   scale_z Scaled data z axis.
+ *
+ * @return
+ *      - ERR_CODE_SUCCESS: Success.
+ *      - Others:           Fail.
+ */
+err_code_t hmc5883l_get_mag_scale(hmc5883l_handle_t handle, float *scale_x, float *scale_y, float *scale_z);
+
+/*
+ * @brief   Set magnetometer bias data.
+ *
+ * @param   handle Handle structure.
+ * @param   bias_x Bias data x axis.
+ * @param   bias_y Bias data y axis.
+ * @param   bias_z Bias data z axis.
+ *
+ * @return
+ *      - ERR_CODE_SUCCESS: Success.
+ *      - Others:           Fail.
+ */
+err_code_t hmc5883l_set_mag_bias(hmc5883l_handle_t handle, int16_t bias_x, int16_t bias_y, int16_t bias_z);
+
+/*
+ * @brief   Set magnetometer bias data.
+ *
+ * @param   handle Handle structure.
+ * @param   bias_x Bias data x axis.
+ * @param   bias_y Bias data y axis.
+ * @param   bias_z Bias data z axis.
+ *
+ * @return
+ *      - ERR_CODE_SUCCESS: Success.
+ *      - Others:           Fail.
+ */
+err_code_t hmc5883l_get_mag_bias(hmc5883l_handle_t handle, int16_t *bias_x, int16_t *bias_y, int16_t *bias_z);
 
 #ifdef __cplusplus
 }
